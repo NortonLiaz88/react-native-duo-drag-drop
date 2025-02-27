bd4d6498986dca2572bc7452fdada9047afcbb26
vitor.norton <vnlima@hub-uea.org>
2025-02-27 13:59:15 -0400

    chore: update package

--- src/WordContext.tsx
+++ src/WordContext.tsx
@@ -1,3 +1,3 @@
 import { createContext } from "react";
 
-export default createContext({ wordHeight: 55, text: "", wordGap: 4, wordsOfKnowledge: [] });
+export default createContext({ wordHeight: 55, text: "", wordGap: 4, wordsOfKnowledge: [] as string[] });
